adc73025ab4e39d370e3b422cc9815530436ee0e
Angga Ari Wijaya <anggadarkprince@gmail.com>
2020-07-11 08:17:09 +0700

    first commit

---
+++ client.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$client = new Zend\Soap\Client('http://localhost/soap/server.php?wsdl');
+
+echo '<pre>';
+print_r($client->getFunctions());
+echo '</pre>';
+
+$result = $client->sayHello(['firstName' => 'World']);
+echo $result->sayHelloResult;
+echo '<br>';
+var_dump($client->getLastRequest());
+
+$result = $client->sayGoodbye(['name' => 'Angga']);
+echo '<pre>';
+print_r(json_decode(json_encode($result->sayGoodbyeResult), true));
+echo '</pre>';
+
+
+$result = $client->memberList();
+echo '<pre>';
+print_r($result->memberListResult);
+echo '</pre>';
+var_dump($client->getLastResponse());
---
+++ server.php
@@ -0,0 +1,117 @@
+<?php
+ini_set("soap.wsdl_cache_enabled", 0);
+require_once __DIR__ . '/vendor/autoload.php';
+
+class Hello
+{
+    /**
+     * Say hello.
+     *
+     * @param string $firstName
+     * @return string $greetings
+     */
+    public function sayHello($firstName)
+    {
+        return 'Hello ' . $firstName;
+    }
+
+    /**
+     * Hi hello.
+     *
+     * @param string $firstName
+     * @param string $lastName
+     * @return string $greetings
+     */
+    public function hiHello($firstName, $lastName)
+    {
+        return 'Hi ' . $firstName . ' ' . $lastName;
+    }
+
+    /**
+     * Say good bye.
+     *
+     * @param string $name
+     * @return array $greetings
+     */
+    public function sayGoodbye($name)
+    {
+        return [
+            'name' => $name,
+            'greeting' => 'Good bye',
+            'message' => 'Good bye, ' . $name,
+        ];
+    }
+
+    /**
+     * Say good bye.
+     *
+     * @return array $greetings
+     */
+    public function memberList()
+    {
+        $obj1 = new \stdClass;
+        $obj1->id = 1;
+        $obj1->name = 'Angga';
+
+        $obj2 = new \stdClass;
+        $obj2->id = 2;
+        $obj2->name = 'Ari';
+
+        return [
+            $obj1, $obj2
+        ];
+
+        /*
+        return [
+            (object)[
+                'id' => 1,
+                'name' => 'Angga',
+            ],
+            (object)[
+                'id' => 2,
+                'name' => 'Ari',
+            ],
+            (object)[
+                'id' => 3,
+                'name' => 'Wijaya',
+            ],
+        ];
+        */
+    }
+}
+
+class MyService
+{
+    /**
+     * Add
+     *
+     * @param int $x
+     * @param int $y
+     * @return int
+     */
+    public function add($x, $y)
+    {
+        return $x + $y;
+    }
+}
+
+$serverUrl = "http://localhost/soap/server.php";
+$server = new Zend\Soap\Server(null, [
+    'uri' => $serverUrl,
+    'cache_wsdl' => WSDL_CACHE_NONE
+]);
+
+if (isset($_GET['wsdl'])) {
+    $soapAutoDiscover = new \Zend\Soap\AutoDiscover(new \Zend\Soap\Wsdl\ComplexTypeStrategy\ArrayOfTypeSequence());
+    $soapAutoDiscover->setBindingStyle(array('style' => 'document'));
+    $soapAutoDiscover->setOperationBodyStyle(array('use' => 'literal'));
+    $soapAutoDiscover->setClass('Hello');
+    $soapAutoDiscover->setUri($serverUrl);
+
+    header("Content-Type: text/xml");
+    echo $soapAutoDiscover->generate()->toXml();
+} else {
+    $soap = new \Zend\Soap\Server($serverUrl . '?wsdl');
+    $soap->setObject(new \Zend\Soap\Server\DocumentLiteralWrapper(new Hello()));
+    $soap->handle();
+}
